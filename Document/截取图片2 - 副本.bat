1d912f181b1d71797c96fd59c23ccd0a411d86e0
HeBianGu <908293466@qq.com>
2019-11-02 15:44:08 +0800

    111

---
+++ Document/截取图片2 - 副本.bat
@@ -0,0 +1,3 @@
+f:
+F:\GitHub\WPF-MediaConverter\Product\Dll\ffmpeg -i E:\Medias\Movie\飘花电影piaohua.com谍影重重2高清1280.rmvb -vf fps=1/60 -t 00:10:00 E:\Medias\Movie\飘花电影piaohua.com谍影重重2高清1280_shotcut%03d.jpg 
+pause
---
+++ Document/截取图片2.bat
@@ -0,0 +1,4 @@
+f:
+cd F:\GitHub\WPF-MediaConverter\Product\Dll 
+ffmpeg  -y -i E:\Medias\Movie\飘花电影piaohua.com谍影重重2高清1280.rmvb -ss 00:01:00 -vframes 1 -f image2 -vcodec png E:\Medias\Movie\飘花电影piaohua.com谍影重重2高清1280_shootcut.png
+pause
